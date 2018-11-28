8830c4c99e7d053df00a1be55d279782a3027398
Diana Pham <dianaphamis@gmail.com>
2018-11-27 23:34:36 -0800

    - Change bg to black - Change text to white

---
+++ script.js
@@ -0,0 +1,11 @@
+var w = document.getElementById("wrapper");
+var s = document.getElementById("word");
+
+w.addEventListener("click", function(){
+
+	w.style.backgroundColor = "black";
+	s.style.color = "white";
+
+
+
+})
